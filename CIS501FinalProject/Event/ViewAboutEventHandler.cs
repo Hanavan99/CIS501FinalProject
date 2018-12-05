2acbbb06dee5fddb2ac0217417231d6d1d1f6924
Hanavan Kuhn <hanavan99@gmail.com>
2018-12-05 17:25:39 -0600

    Added ProgramInfo classes

---
+++ CIS501FinalProject/Event/ViewAboutEventHandler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CIS501FinalProject.Event
+{
+    public class ViewAboutEventHandler : IEventHandler
+    {
+        public void HandleEvent(object[] args)
+        {
+            
+        }
+    }
+}
---
+++ CIS501FinalProject/ProgramInfo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CIS501FinalProject
+{
+    public static class ProgramInfo
+    {
+
+        public static string ProgramName
+        {
+            get {
+                return "Schedule Verification Tool";
+            }
+        }
+
+        public static string ProgramVersion
+        {
+            get {
+                return "1.0.0";
+            }
+        }
+    }
+}
